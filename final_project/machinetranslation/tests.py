da0bafd8654332b95433a0da5b9b65ea399641b7
skulnik <skulnik84@gmail.com>
2023-03-10 14:30:44 -0500

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,34 +1,22 @@
-import json
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-apikey = os.environ['apikey']
-url = os.environ['url']
-
-authenticator = IAMAuthenticator('kQg2x7HU93nHKQ37bf9Nkid-IToC6CnplRlpDZdwHgNz')
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
-
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/df6b9a3c-bd91-4fe8-b687-f60f8754a79c')
+"""Unittests for Translator.py"""
 
 import unittest
+from translator import english_to_french, french_to_english
 
-from translator import englishToFrench, frenchToEnglish
+class TestEnglishToFrench(unittest.TestCase):
 
-class TestenglishToFrench(unittest.TestCase):
+    """Tests English to French translation"""
+
     def test1(self):
-        self.assertEqual(englishToFrench('Hello'), 'Bonjour')
-        self.assertEqual(englishToFrench(''), '')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertEqual(english_to_french(''), '')
 
-class TestfrenchToEnglish(unittest.TestCase):
+class TestFrenchToEnglish(unittest.TestCase):
+
+    """Tests French to English translation"""
+
     def test1(self):
-        self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
-        self.assertEqual(frenchToEnglish(''), '')
-               
-    
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertEqual(french_to_english(''), '')
+
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,11 @@
+"""This module translates from French to English
+   or English to French
+   using IBM Watson LanguageTranslatorV3"""
+
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,20 +13,30 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('kQg2x7HU93nHKQ37bf9Nkid-IToC6CnplRlpDZdwHgNz')
+authenticator = IAMAuthenticator(
+    'kQg2x7HU93nHKQ37bf9Nkid-IToC6CnplRlpDZdwHgNz')
 language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
+    version ='2018-05-01',
+    authenticator = authenticator
 )
 
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/df6b9a3c-bd91-4fe8-b687-f60f8754a79c')
+language_translator.set_service_url(
+    'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/df6b9a3c-bd91-4fe8-b687-f60f8754a79c')
 
-def englishToFrench(englishText):
-    translation= language_translator.translate(text='englishText', model_id='en-fr').get_result()
-    frenchText= translation['translations'][0]['translation']
-    return frenchText
+def english_to_french(english_text):
 
-def frenchToEnglish(frenchText):
-    translation= language_translator.translate(text='frenchText', model_id='fr-en').get_result()
-    englishText= translation['translations'][0]['translation']
-    return englishText
+    """Translates English text to French text"""
+
+    translation = language_translator.translate(
+        text = english_text, model_id = 'en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+
+    """Translates French text to English text"""
+
+    translation = language_translator.translate(
+        text = french_text, model_id ='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
